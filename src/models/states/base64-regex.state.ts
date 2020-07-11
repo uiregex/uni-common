1fba61c6be558f9302fb10ea4733a5060157640b
vitaliy.gavriliyk <vitaliy.gavriliuk@gmail.com>
2020-07-11 11:25:06 +0300

    fix regex

--- src/models/states/base64-regex.state.ts
+++ src/models/states/base64-regex.state.ts
@@ -1,1 +1,1 @@
-export const UniBase64Regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2})?$/g;
+export const UniBase64Regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2})?$/;

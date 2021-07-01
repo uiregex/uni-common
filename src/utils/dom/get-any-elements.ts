5a2860972c98d59a2c0524a2740cb57504553c78
UiWebKit <uniwebkit@gmail.com>
2021-07-01 13:01:44 +0300

    improve: 'frames' -> 'frame' and common shadow block

--- src/utils/dom/get-any-elements.ts
+++ src/utils/dom/get-any-elements.ts
@@ -1,33 +1,39 @@
 interface UniGetAnyElements {
   selector: string;
   shadow?: boolean;
-  frames?: boolean;
+  frame?: boolean;
 }
 
 export function uniGetAnyElements(data: UniGetAnyElements): Element[] {
-  const { selector, shadow, frames } = data;
+  const { selector, shadow, frame } = data;
   const elements = Array.from(window.top.document.body.querySelectorAll(selector) || []);
 
   if (shadow) {
-    Array.from(window.top.document.body.getElementsByTagName('*') || [])
-      .filter(element => element.shadowRoot)
-      .forEach(element => {
-        elements.push(...Array.from(element.shadowRoot.querySelectorAll(selector) || []));
-      });
+    elements.push(...getShadowInnerElements(window.top.document.body.getElementsByTagName('*'), selector));
   }
 
-  if (frames) {
+  if (frame) {
     Array.from(window.top.document.body.querySelectorAll('iframe') || [])
-      .forEach((frame) => {
+      .forEach((frame: HTMLIFrameElement): void => {
         elements.push(...Array.from(frame.contentDocument?.body?.querySelectorAll(selector) || []));
 
-        Array.from(frame.contentDocument?.body?.getElementsByTagName('*') || [])
-          .filter(element => element.shadowRoot)
-          .forEach(element => {
-            elements.push(...Array.from(element.shadowRoot.querySelectorAll(selector) || []));
-          });
+        if (shadow) {
+          elements.push(...getShadowInnerElements(frame.contentDocument?.body?.getElementsByTagName('*'), selector));
+        }
       });
   }
+
+  return elements;
+}
+
+function getShadowInnerElements(elems: HTMLCollectionOf<Element>, selector: string): Element[] {
+  const elements = [];
+
+  Array.from(elems || [])
+    .filter((element: HTMLElement): boolean => !!element.shadowRoot)
+    .forEach((element: HTMLElement): void => {
+      elements.push(...Array.from(element.shadowRoot.querySelectorAll(selector) || []));
+    });
 
   return elements;
 }

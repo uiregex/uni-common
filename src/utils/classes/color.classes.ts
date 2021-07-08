e4cec479b0bbb16c8945dc78cbfb1eda5837a944
UiWebKit <uniwebkit@gmail.com>
2021-07-08 18:39:54 +0300

    fix: color switch

--- src/utils/classes/color.classes.ts
+++ src/utils/classes/color.classes.ts
@@ -1,5 +1,10 @@
 import { UniColorClasses, UniObject } from '../../models';
 
 export function uniColorClass(color: string): UniObject<boolean> {
-  return { [UniColorClasses[color]]: !!UniColorClasses[color] };
+  return {
+    [UniColorClasses.primary]: color === 'primary',
+    [UniColorClasses.success]: color === 'success',
+    [UniColorClasses.accent]: color === 'accent',
+    [UniColorClasses.warn]: color === 'warn'
+  };
 }

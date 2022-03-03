a6b1e953a758b70102e764b02c1a994138319dd9
UiWebKit <uniwebkit@gmail.com>
2022-03-03 13:40:08 +0200

    fix isJSON

--- src/utils/coercion/is.ts
+++ src/utils/coercion/is.ts
@@ -24,12 +24,13 @@ export function isEmpty(value: any): boolean {
 }
 
 export function isJSON(value: any): boolean {
-  const optional = `(?:".+"[:].+)?`;
-  const regexArray = new RegExp(`^[\\[].*[\\]]$`, 'g');
-  const regexObject = new RegExp(`^[{]${optional}[}]$`, 'g');
-  const regexArrayObject = new RegExp(`^[\\[][{]${optional}[}][\\]]$`, 'g');
+  try {
+    JSON.parse(value);
+  } catch (e) {
+    return false;
+  }
 
-  return isString(value) && (regexObject.test(value) || regexArray.test(value) || regexArrayObject.test(value));
+  return isString(value);
 }
 
 export function isBind(value: any, start = '{{', end = '}}'): boolean {
